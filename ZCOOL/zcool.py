14219b156ebeb580c4897262c99470676bfd2151
JohnsonChou <shenfachou@126.com>
2016-03-21 23:09:04 +0800

    加入注释
    为文件加入详细注释

--- ZCOOL/zcool.py
+++ ZCOOL/zcool.py
@@ -8,55 +8,59 @@ import os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+# 图片下载器
 class ImageDownload(object):
+
     def download(self,url,path):
         content = urllib2.urlopen(url)
 
-        soup = BeautifulSoup(content.read(),"html.parser")
+        soup    = BeautifulSoup(content.read(),"html.parser")
 
-        uls = soup.find_all("img",class_="mb10")
+        uls     = soup.find_all("img",class_="mb10")
 
         deepIndex = 0
 
         for item in uls:
 
             deepIndex += 1
-
+            # 写入图片
             print item["src"]
-            u = urllib.urlopen(item["src"])
+
+            u    = urllib.urlopen(item["src"])
             data = u.read()
-            f = open(path+str(deepIndex)+".jpg", 'wb')
+            f    = open(path+str(deepIndex)+".jpg", 'wb')
             f.write(data)
             f.close()
 
 
 
-# 网址
-index = 0
+# 网址构造参数
+index           = 0
 
-basicUrl = "http://www.zcool.com.cn/index!"
+basicUrlTail    = "http://www.zcool.com.cn/index!"
 
-basicUrlTail = ".html#mainList"
+basicUrlTail    = ".html#mainList"
 
-# 创建文件夹
-isExists=os.path.exists("zcool")
+# 创建根文件夹
+isExists = os.path.exists("zcool")
 
 if not isExists:
     os.makedirs("zcool")
 
 # 进入循环
-for a in range(1,100):
+for a in range(1,2):
+    # 打印页码
     print a
 
-    index += 1
+    index   += 1
 
-    url = basicUrl + str(index)+basicUrlTail
+    url     = basicUrl + str(index)+basicUrlTail
 
     content = urllib2.urlopen(url)
 
-    soup = BeautifulSoup(content.read(),"html.parser")
+    soup    = BeautifulSoup(content.read(),"html.parser")
 
-    uls = soup.find_all("img",height="188",width="250")
+    uls     = soup.find_all("img",height="188",width="250")
 
     deepIndex = 0
 
@@ -64,14 +68,18 @@ for a in range(1,100):
 
         deepIndex += 1
 
+        # 构造文件夹名称
         title = "ImagesFile" + "section" + str(index) + "row" + str(deepIndex)
 
+        # 创建文件夹和写入简介.text文件
         os.makedirs("zcool/"+title)
         file = open("zcool/"+title+"/introduction.text","a")
 
+        # 写入改图片数组的标题和连接
         file.write(item.parent["title"] + "\n")
         file.write(item.parent["href"])
 
+        # 创建图片下载器，去下载该item下的图片集合并保存到简介文件相同的目录下
         downloader = ImageDownload()
         downloader.download(item.parent["href"],"zcool/"+title+"/")
 
